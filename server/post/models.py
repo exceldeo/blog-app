7e7237a2f1c02b327792c6ce21670941b067ce21
exceldeo <excel.deo99@gmail.com>
2024-03-27 18:02:38 +0800

    feat: create customised field

--- server/post/models.py
+++ server/post/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
+class CharField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 65000)
+        super(CharField, self).__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return 'text'
+
+    def south_field_triple(self):
+        """Only necessary if using South migrations, which you should."""
+        from south.modelsinspector import introspector
+        field_class = self.__class__.__module__ + "." + self.__class__.__name__
+        args, kwargs = introspector(self)
+        return (field_class, args, kwargs)
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = CharField()
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
